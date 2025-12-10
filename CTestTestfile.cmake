7cde57eb701aa041b30b60fd920d3e1b06f9c87b
Roman Varvarych <romanvarvarych512@gmail.com>
2025-12-10 12:29:58 +0200

    Add math logic and Google Test integration

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/repos/csad2526ki402varvarychroman3
+# Build directory: D:/repos/csad2526ki402varvarychroman3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("D:/repos/csad2526ki402varvarychroman3/unit_tests[1]_include.cmake")
+subdirs("_deps/gtest-build")
